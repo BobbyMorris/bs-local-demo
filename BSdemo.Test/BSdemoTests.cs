1798a35874c4c79bdfdd7c5715d83c32cd3e93eb
Bobby Morris <bobbymorris@gmail.com>
2017-03-09 14:14:05 -0500

    Basic project showing attempt to use BrowserStackLocal

---
+++ BSdemo.Test/BSdemoTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BSdemo.Test
+{
+    [TestFixture("single", "chrome")]
+    public class SingleTest : BrowserStackNUnitTest
+    {
+        public SingleTest(string profile, string environment) : base(profile, environment) { }
+
+        [Test]
+        public void SearchGoogle()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/ncr");
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("BrowserStack");
+            query.Submit();
+            System.Threading.Thread.Sleep(5000);
+            Assert.AreEqual("BrowserStack - Google Search", driver.Title);
+        }
+    }
+
+    [TestFixture("local", "chrome")]
+    public class LocalTest : BrowserStackNUnitTest
+    {
+        public LocalTest(string profile, string environment) : base(profile, environment) { }
+
+        [Test]
+        public void HealthCheck()
+        {
+            driver.Navigate().GoToUrl("http://localhost:51714/");
+            Assert.AreEqual("ASP.NET", driver.FindElement(By.ClassName("jumbotron")).Text);
+        }
+    }
+}
